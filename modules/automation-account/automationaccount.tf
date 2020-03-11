34cb6e6014ad404a1b01e241ff16916d7d71d9e3
Erwan Letort <erwan.letort@fr.clara.net>
2020-03-11 11:05:25 +0100

    AZ-208 : Init automation account submodule

---
+++ modules/automation-account/automationaccount.tf
@@ -0,0 +1,17 @@
+# Automation Account
+resource "azurerm_automation_account" "automation-account" {
+  name                = local.automation_account_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku_name = var.automation_account_sku
+  tags     = merge(local.default_tags, var.extra_tags)
+}
+
+# Link automation account to log analytics workspace
+resource "azurerm_log_analytics_linked_service" "link_workspace_automation" {
+  count               = var.log_analytics_workspace_name == "" ? 0 : 1
+  resource_group_name = coalesce(var.law_resource_group_name, var.resource_group_name)
+  workspace_name      = var.log_analytics_workspace_name
+  resource_id         = azurerm_automation_account.automation-account.id
+}
---
+++ modules/automation-account/variables.tf
@@ -0,0 +1,54 @@
+variable "client_name" {
+  description = "Client name/account used in naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Project environment"
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "automation_account_sku" {
+  description = "Automation account Sku"
+  type        = string
+  default     = "Basic"
+}
+
+variable "custom_automation_account_name" {
+  description = "Automation account custom name"
+  type        = string
+  default     = ""
+}
+
+variable "law_resource_group_name" {
+  description = "Resource group of Log Analytics Workspace that will be connected with the automation account (default is the same RG that the one hosting the automation account)"
+  type        = string
+  default     = ""
+}
+
+variable "log_analytics_workspace_name" {
+  description = "Log Analytics Workspace that will be connected with the automation account"
+  type        = string
+  default     = ""
+}
+
+variable "extra_tags" {
+  description = "Extra tags to add"
+  type        = map(string)
+  default     = {}
+}
