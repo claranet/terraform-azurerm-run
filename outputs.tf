5be99374af4c3283cafd122ce5500b9bc5b7088f
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-10-16 12:28:39 +0200

    AZ-320 Add more outputs for automation account

--- outputs.tf
+++ outputs.tf
@@ -43,3 +43,20 @@ output "automation_account_id" {
   description = "Azure Automation Account ID"
   value       = module.automation-account.automation_account_id
 }
+
+output "automation_account_dsc_primary_access_key" {
+  description = "Azure Automation Account DSC Primary Acess Key"
+  value = module.automation-account.automation_account_dsc_primary_access_key
+  sensitive = true
+}
+
+output "automation_account_dsc_secondary_access_key" {
+  description = "Azure Automation Account DSC Secondary Acess Key"
+  value = module.automation-account.automation_account_dsc_secondary_access_key
+  sensitive = true
+}
+
+output "automation_account_dsc_server_endpoint" {
+  description = "Azure Automation Account DSC Server Endpoint"
+  value = module.automation-account.automation_account_dsc_server_endpoint
+}
