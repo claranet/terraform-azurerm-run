63cea31156ea22bc036eeca096fb1950eb23e971
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-10-16 12:30:19 +0200

    AZ-320 terraform fmt

--- modules/automation-account/outputs.tf
+++ modules/automation-account/outputs.tf
@@ -13,17 +13,17 @@ output "automation_account_id" {
 
 output "automation_account_dsc_primary_access_key" {
   description = "Azure Automation Account DSC Primary Acess Key"
-  value = azurerm_automation_account.automation-account.dsc_primary_access_key
-  sensitive = true
+  value       = azurerm_automation_account.automation-account.dsc_primary_access_key
+  sensitive   = true
 }
 
 output "automation_account_dsc_secondary_access_key" {
   description = "Azure Automation Account DSC Secondary Acess Key"
-  value = azurerm_automation_account.automation-account.dsc_secondary_access_key
-  sensitive = true
+  value       = azurerm_automation_account.automation-account.dsc_secondary_access_key
+  sensitive   = true
 }
 
 output "automation_account_dsc_server_endpoint" {
   description = "Azure Automation Account DSC Server Endpoint"
-  value = azurerm_automation_account.automation-account.dsc_server_endpoint
+  value       = azurerm_automation_account.automation-account.dsc_server_endpoint
 }
--- outputs.tf
+++ outputs.tf
@@ -46,17 +46,17 @@ output "automation_account_id" {
 
 output "automation_account_dsc_primary_access_key" {
   description = "Azure Automation Account DSC Primary Acess Key"
-  value = module.automation-account.automation_account_dsc_primary_access_key
-  sensitive = true
+  value       = module.automation-account.automation_account_dsc_primary_access_key
+  sensitive   = true
 }
 
 output "automation_account_dsc_secondary_access_key" {
   description = "Azure Automation Account DSC Secondary Acess Key"
-  value = module.automation-account.automation_account_dsc_secondary_access_key
-  sensitive = true
+  value       = module.automation-account.automation_account_dsc_secondary_access_key
+  sensitive   = true
 }
 
 output "automation_account_dsc_server_endpoint" {
   description = "Azure Automation Account DSC Server Endpoint"
-  value = module.automation-account.automation_account_dsc_server_endpoint
+  value       = module.automation-account.automation_account_dsc_server_endpoint
 }
