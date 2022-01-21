83f42139116dd95931aa134702f0ecdb55ef0fe1
Maxime Poullain <maxime.poullain@fr.clara.net>
2022-01-21 11:21:40 +0100

    AZ-672: lint queries

--- modules/monitoring_function/locals.tf
+++ modules/monitoring_function/locals.tf
@@ -78,51 +78,51 @@ locals {
       MetricName : "fame.azure.frontdoor.http_status_code"
       MetricType : "gauge"
       Query : <<EOQ
-          AzureDiagnostics
-          | where ResourceProvider == "MICROSOFT.CDN"
-          | where Category == "FrontDoorAccessLog"
-          | where TimeGenerated > ago(20m)
-          | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), http_status_code=tostring(toint(httpStatusCode_d)), azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
-          | order by timestamp desc
-        EOQ
+        AzureDiagnostics
+        | where ResourceProvider == "MICROSOFT.CDN"
+        | where Category == "FrontDoorAccessLog"
+        | where TimeGenerated > ago(20m)
+        | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), http_status_code=tostring(toint(httpStatusCode_d)), azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
+        | order by timestamp desc
+      EOQ
     },
 
     frontdoor_health_probe_logs : {
       MetricName : "fame.azure.frontdoor.health_probe_logs"
       MetricType : "gauge"
       Query : <<EOQ
-          AzureDiagnostics
-          | where ResourceProvider == "MICROSOFT.CDN"
-          | where Category == "FrontDoorHealthProbeLog"
-          | where TimeGenerated > ago(20m)
-          | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), result=(result_s), http_status_code=tostring(toint(httpStatusCode_d)), origin_name=(originName_s),azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
-          | order by timestamp desc
-        EOQ
+        AzureDiagnostics
+        | where ResourceProvider == "MICROSOFT.CDN"
+        | where Category == "FrontDoorHealthProbeLog"
+        | where TimeGenerated > ago(20m)
+        | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), result=(result_s), http_status_code=tostring(toint(httpStatusCode_d)), origin_name=(originName_s),azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
+        | order by timestamp desc
+      EOQ
     },
 
     frontdoor_waf_logs : {
       MetricName : "fame.azure.frontdoor.waf_logs"
       MetricType : "gauge"
       Query : <<EOQ
-          AzureDiagnostics
-          | where ResourceProvider == "MICROSOFT.CDN"
-          | where Category == "FrontDoorWebApplicationFirewallLog"
-          | where TimeGenerated > ago(20m)
-          | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), action=(action_s), policy=(policy_s), host=(host_s), azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
-          | order by timestamp desc
-        EOQ
+        AzureDiagnostics
+        | where ResourceProvider == "MICROSOFT.CDN"
+        | where Category == "FrontDoorWebApplicationFirewallLog"
+        | where TimeGenerated > ago(20m)
+        | summarize metric_value=count() by timestamp=bin(TimeGenerated, 1m), action=(action_s), policy=(policy_s), host=(host_s), azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
+        | order by timestamp desc
+      EOQ
     },
 
     frontdoor_cache_rate : {
       MetricName : "fame.azure.frontdoor.cache_rate"
       MetricType : "gauge"
       Query : <<EOQ
-          AzureDiagnostics 
-          | where Category == "FrontDoorAccessLog"
-          | where TimeGenerated > ago(20m)
-          | summarize metric_value = tostring(toint((todouble(countif(cacheStatus_s == "HIT" or cacheStatus_s == "REMOTE_HIT")) / (todouble(countif(cacheStatus_s == "HIT" or cacheStatus_s == "REMOTE_HIT")) + todouble(countif(cacheStatus_s == "MISS" or cacheStatus_s == "CONFIG_NOCACHE")))) * 100)) by timestamp=bin(TimeGenerated, 1m), endpoint=endpoint_s, azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
-          | order by timestamp desc
-        EOQ
+        AzureDiagnostics 
+        | where Category == "FrontDoorAccessLog"
+        | where TimeGenerated > ago(20m)
+        | summarize metric_value = tostring(toint((todouble(countif(cacheStatus_s == "HIT" or cacheStatus_s == "REMOTE_HIT")) / (todouble(countif(cacheStatus_s == "HIT" or cacheStatus_s == "REMOTE_HIT")) + todouble(countif(cacheStatus_s == "MISS" or cacheStatus_s == "CONFIG_NOCACHE")))) * 100)) by timestamp=bin(TimeGenerated, 1m), endpoint=endpoint_s, azure_resource_name=Resource, azure_resource_group_name=ResourceGroup, subscription_id=SubscriptionId
+        | order by timestamp desc
+      EOQ
     },
   }
 }
