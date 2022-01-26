2b8bff1423f2a032fe149ac4a5238d30c2d40490
F PERAUD <frederic.peraud@fr.clara.net>
2022-01-26 14:47:04 +0100

    AZ-674: fix sas token module call

--- modules/logs/r-logs-sas.tf
+++ modules/logs/r-logs-sas.tf
@@ -1,10 +1,12 @@
 module "log_sas_token" {
-  source  = "claranet/storage-sas-token/azurerm"
-  version = "4.1.0"
+  # source  = "claranet/storage-sas-token/azurerm"
+  # version = "4.1.0"
 
-  enabled              = true
-  resource_group_name  = var.resource_group_name
-  storage_account_name = azurerm_storage_account.storage_logs.name
+  source = "git@git.fr.clara.net:claranet/projects/cloud/azure/terraform/modules/storage-sas-token.git?ref=AZ-674_fix_sa_ds_bug"
+
+  storage_account_name                  = azurerm_storage_account.storage_logs.name
+  storage_account_connection_string     = azurerm_storage_account.storage_logs.primary_connection_string
+  storage_account_primary_blob_endpoint = azurerm_storage_account.storage_logs.primary_blob_endpoint
 
   sas_token_expiry    = var.logs_storage_account_sas_expiry
   permissions_account = "wlacu"
