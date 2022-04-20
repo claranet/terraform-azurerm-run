4f19002835a9b3eb5257c3b5bba4b19a90b10993
Rémi Beaufils <remi.beaufils@fr.clara.net>
2022-04-20 10:36:59 +0200

    AZ-726: Fix version of AzureRM provider due to bug introduced in v2.99

--- modules/logs/versions.tf
+++ modules/logs/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.1"
+      version = "~> 2.1, < 2.99"
     }
     null = {
       source  = "hashicorp/null"
