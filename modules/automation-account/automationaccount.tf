7fe1617ac7c9ba0e195e74b31f77ae7011f16fef
Sergen TANGUC <sergen-tanguc.ext@fr.clara.net>
2022-07-19 15:40:02 +0200

    AZ-800 Fix too many tags for automation account

--- modules/automation-account/automationaccount.tf
+++ modules/automation-account/automationaccount.tf
@@ -5,7 +5,7 @@ resource "azurerm_automation_account" "automation_account" {
   resource_group_name = var.resource_group_name
 
   sku_name = var.automation_account_sku
-  tags     = merge(local.default_tags, var.extra_tags, var.automation_account_extra_tags)
+  tags     = local.chunked_tags
 
   dynamic "identity" {
     for_each = var.automation_account_identity_type != null ? ["fake"] : []
--- modules/automation-account/locals-tags.tf
+++ modules/automation-account/locals-tags.tf
@@ -3,4 +3,9 @@ locals {
     env   = var.environment
     stack = var.stack
   } : {}
+
+  merged_tags = merge(local.default_tags, var.extra_tags, var.automation_account_extra_tags)
+  chunked_tags_keys = chunklist(keys(local.merged_tags), 15)
+  chunked_tags_values = chunklist(values(local.merged_tags), 15)
+  chunked_tags = zipmap(local.chunked_tags_keys[0], local.chunked_tags_values[0])
 }
