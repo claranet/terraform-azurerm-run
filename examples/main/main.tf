6f472c300975bc876f56dcf469ed8ee75d384823
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2022-07-27 16:43:09 +0200

    AZ-308: Fix CI

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.0.0"
+      version = "~> 3.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
   }
 }
