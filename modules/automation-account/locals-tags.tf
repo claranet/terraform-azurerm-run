c0b95a9fcea5a532c5252c9ee89bb913ef8cfee3
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-10-20 17:31:10 +0200

    AZ-800: Truncate to 14 tags maximum, merge all others as a JSON string

--- modules/automation-account/locals-tags.tf
+++ modules/automation-account/locals-tags.tf
@@ -6,7 +6,10 @@ locals {
 
   merged_tags = merge(local.default_tags, var.extra_tags, var.automation_account_extra_tags)
 
-  curtailed_tags = {
-    for key in slice(keys(local.merged_tags), 0, length(local.merged_tags) >= 15 ? 15 : length(local.merged_tags)) : key => lookup(local.merged_tags, key)
+  truncated_tags = {
+    for key in slice(keys(local.merged_tags), 0, length(local.merged_tags) >= 14 ? 14 : length(local.merged_tags)) : key => lookup(local.merged_tags, key)
   }
+  curtailed_tags = length(local.merged_tags) >= 14 ? merge(local.truncated_tags, {
+    "extra_tags" = jsonencode({ for key in slice(keys(local.merged_tags), 14, length(local.merged_tags)) : key => lookup(local.merged_tags, key) })
+  }) : local.merged_tags
 }
