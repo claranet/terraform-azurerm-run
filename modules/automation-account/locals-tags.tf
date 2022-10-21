f628915bdd5e0984a1428ced9731786859aaa25c
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-10-21 09:29:42 +0200

    AZ-800: Optimize code

--- modules/automation-account/locals-tags.tf
+++ modules/automation-account/locals-tags.tf
@@ -6,10 +6,10 @@ locals {
 
   merged_tags = merge(local.default_tags, var.extra_tags, var.automation_account_extra_tags)
 
+  # Automation Account Azure resource can have only 15 tags maximum
   truncated_tags = {
-    for key in slice(keys(local.merged_tags), 0, length(local.merged_tags) >= 14 ? 14 : length(local.merged_tags)) : key => lookup(local.merged_tags, key)
+    for key in chunklist(keys(local.merged_tags), 14)[0] : key => lookup(local.merged_tags, key)
   }
-  curtailed_tags = length(local.merged_tags) >= 14 ? merge(local.truncated_tags, {
-    "extra_tags" = jsonencode({ for key in slice(keys(local.merged_tags), 14, length(local.merged_tags)) : key => lookup(local.merged_tags, key) })
-  }) : local.merged_tags
+  # We keep the 14 first tags, serialize all the others in a 15th one (JSON encoded)
+  curtailed_tags = merge(local.truncated_tags, try({ "extra_tags" = jsonencode({ for key in slice(keys(local.merged_tags), 14, length(local.merged_tags)) : key => lookup(local.merged_tags, key) }) }, {}))
 }
