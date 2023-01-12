c3138aa6750679cfddc1652eb6fdfa436f85769e
Jérôme Respaut <jerome.respaut@fr.clara.net>
2023-01-12 14:51:12 +0100

    AZ-926: Split outputs

--- outputs-dcr.tf
+++ outputs-dcr.tf
@@ -15,12 +15,3 @@ output "data_collection_rule" {
   description = "Azure Monitor Data Collection Rule object."
   value       = one(module.vm_monitoring[*].data_collection_rule)
 }
-
-###############################
-# Azure Update Manamagent Center outputs
-###############################
-
-output "maintenance_configurations" {
-  description = "Maintenance Configurations informations."
-  value       = try(module.update_management_center["enabled"].maintenance_configurations, null)
-}
