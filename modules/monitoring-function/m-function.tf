e80251c4c1f1b509b55bfccb04e6b58f019f85a6
OneGit service account - Renovate <fr-ops4ops+bot-renovate@fr.clara.net>
2025-10-27 15:23:15 +0000

    chore(deps): update terraform claranet/function-app/azurerm to ~> 8.7.0

--- modules/monitoring-function/m-function.tf
+++ modules/monitoring-function/m-function.tf
@@ -1,6 +1,6 @@
 module "function" {
   source  = "claranet/function-app/azurerm"
-  version = "~> 8.6.0"
+  version = "~> 8.7.0"
 
   client_name         = var.client_name
   environment         = var.environment
