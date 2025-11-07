4b412567cdc04959f5d0190533497a8838b5d5fa
Laurent Piroelle <laurent.piroelle@claranet.com>
2025-11-07 09:49:27 +0100

    feat(fame): add heartbeat metric log query

--- modules/monitoring-function/locals.tf
+++ modules/monitoring-function/locals.tf
@@ -2,6 +2,7 @@ locals {
   extra_dimensions = join(",", [for k, v in merge(local.default_tags, var.metrics_extra_dimensions) : format("%s=%s", k, v)])
 
   log_queries = merge(
+    local.log_queries_heartbeat,
     local.log_queries_appgw,
     local.log_queries_backup,
     local.log_queries_updates,
