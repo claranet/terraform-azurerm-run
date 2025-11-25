7e3832888bccf86f8da2842969b32855f52cac7c
Jérôme Respaut <jerome.respaut@claranet.com>
2025-11-25 15:09:23 +0100

    docs: 📚️ bump Update Center example

--- examples/update-center/modules.tf
+++ examples/update-center/modules.tf
@@ -3,9 +3,12 @@ module "update_management" {
   source  = "claranet/run/azurerm//modules/update-center"
   version = "x.x.x"
 
-  location            = module.azure_region.location
-  environment         = var.environment
-  stack               = var.stack
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  client_name    = var.client_name
+  environment    = var.environment
+  stack          = var.stack
+
   resource_group_name = module.rg.name
 
   maintenance_configurations = [
